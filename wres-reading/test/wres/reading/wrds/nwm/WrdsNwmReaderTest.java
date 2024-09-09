1b13970e99f48605955d9f7113ba15d75952c35b
EvanPagryzinski-NOAA <epagryzinski@gmail.com>
2024-09-09 13:03:28 -0400

    Update WrdsNwmReaderTest.java

--- wres-reading/test/wres/reading/wrds/nwm/WrdsNwmReaderTest.java
+++ wres-reading/test/wres/reading/wrds/nwm/WrdsNwmReaderTest.java
@@ -15,6 +15,7 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockserver.integration.ClientAndServer;
